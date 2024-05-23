6d4133c15d7075e981d8bd84cd7016ca31ea5225
dpetelko <dpetelko@mail.ru>
2024-05-23 19:31:49 +0300

    Add endpoint

--- HarmonyBookingWatcher/Controllers/MainController.cs
+++ HarmonyBookingWatcher/Controllers/MainController.cs
@@ -13,53 +13,8 @@ public class MainController : ControllerBase
     public MainController() { }
 
     [HttpGet]
-    public async Task Get()
+    public async Task<ActionResult> Get()
     {
-        var client = new TelegramClient(21045577, "ffd2152e5271c5910495eeb813c8b1a5");
-        await client.ConnectAsync();
-        
-        // var hash = await client.SendCodeRequestAsync("79622765272");
-        // var code = "26515"; // you can change code in debugger
-        //
-        // var user = await client.MakeAuthAsync("79622765272", hash, code);
-
-        // //get available contacts
-        // var result = await client.GetContactsAsync();
-        //
-        // //find recipient in contacts
-        // var user12 = result.Users
-        //     .Where(x => x.GetType() == typeof (TLUser))
-        //     .Cast<TLUser>()
-        //     .Select(x => x.Phone)
-        //     .ToList();
-        //
-        // Console.WriteLine(JsonConvert.SerializeObject(user12, Formatting.Indented));
-        //
-        // //find recipient in contacts
-        // var user1 = result.Users
-        //     .Where(x => x.GetType() == typeof (TLUser))
-        //     .Cast<TLUser>()
-        //     .FirstOrDefault(x => x.Phone == "79182182426");
-	       //
-        // //send message
-        // await client.SendMessageAsync(new TLInputPeerUser() {UserId = user1.Id}, "Дима Петелько охуенный программист!!!");
-
-//Get dialogs
-        var dialogs = await client.GetUserDialogsAsync();
-
-//get user chats 
-        var chats = ((TeleSharp.TL.Messages.TLDialogsSlice)dialogs).Chats;
-
-//find channel by title
-        var tlChannel = chats.Where(_ => _.GetType() == typeof(TLChannel))
-            .Select(_=>(TLChannel)_)
-            .Where(_=>_.Title.Contains("HarmonyKrasnodar"))
-            .FirstOrDefault();
-//send message
-        await client.SendMessageAsync(new TLInputPeerChannel()
-                { ChannelId = tlChannel.Id, AccessHash =(long)tlChannel.AccessHash },
-            "OUR_MESSAGE");
-        
-        
+        return Ok("I'm OK");
     }
 }
