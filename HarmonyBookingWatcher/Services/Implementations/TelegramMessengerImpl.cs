6d8d09a0c114d655eb5fc418af4b97063c5de58b
dpetelko <dpetelko@mail.ru>
2024-05-29 20:41:03 +0300

    Испралено на отправку от бота

--- HarmonyBookingWatcher/Services/Implementations/TelegramMessengerImpl.cs
+++ HarmonyBookingWatcher/Services/Implementations/TelegramMessengerImpl.cs
@@ -1,4 +1,7 @@
+using System.Net;
+using System.Text;
 using HarmonyBookingWatcher.Services.Interfaces;
+using Telegram.Bot;
 using TeleSharp.TL;
 using TLSharp.Core;
 
@@ -16,6 +19,52 @@ public class TelegramMessengerImpl : IMessenger
     public async Task Send(string message)
     {
         _logger.LogWarning($"Начало отправки сообщения");
+        
+        
+        var Bot = new Telegram.Bot.TelegramBotClient("7413747352:AAFi-qdowWJ76QSrIzXI9D4d7AVU--jwIFU"); 
+        await Bot.SendTextMessageAsync(-1002005329889, message);
+        return;
+        
+        string retval = string.Empty;
+        string token = "7413747352:AAFi-qdowWJ76QSrIzXI9D4d7AVU--jwIFU";
+        string chatId = "2005329889";
+        string url = $"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatId}&text={message}";
+
+        using(var webClient = new WebClient())
+        {
+            retval = webClient.DownloadString(url);
+        }
+        Console.WriteLine(retval);
+        return;
+        
+        
+        string urlString = "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}";
+        string apiToken = "7413747352:AAFi-qdowWJ76QSrIzXI9D4d7AVU--jwIFU";
+        //string chatId = "2005329889";
+        urlString = String.Format(urlString, apiToken, chatId, message);
+        WebRequest request = WebRequest.Create(urlString);
+        Stream rs = request.GetResponse().GetResponseStream();
+        StreamReader reader = new StreamReader(rs);
+        string line = "";
+        StringBuilder sb = new StringBuilder();
+        while (line != null)
+        {
+            line = reader.ReadLine();
+            if (line != null)
+                sb.Append(line);
+        }
+        string response = sb.ToString();
+        
+        Console.WriteLine(response);
+
+        return;
+        
+        
+        
+        
+        
+        
+        
         var client = new TelegramClient(21045577, "ffd2152e5271c5910495eeb813c8b1a5");
         await client.ConnectAsync();
         
